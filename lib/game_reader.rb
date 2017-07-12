86cf17116200057748366024818ee5e09e4954c3
liambarstad <liambarstad@gmail.com>
2017-07-12 09:48:53 -0600

    First revision

--- lib/game_reader.rb
+++ lib/game_reader.rb
@@ -1,3 +1,45 @@
+require '../lib/scrabble.rb'
 class GameReader
+  include Scrabble::Points
 
+  def score(str)
+    score = check_if_nil(str)
+    if score != 0
+      read_score(str)
+    else
+      score
+    end
+  end
+
+  def score_with_multipliers(str, arr, multiplier=1)
+    score = check_if_nil(str)
+    if score != 0
+      score = read_score_with_multipliers(str, arr)
+    end
+    score *= multiplier
+  end
+
+  def check_if_nil(str)
+    if str.nil?
+      0
+    else
+      str
+    end
+  end
+
+  def read_score(str)
+    score = 0
+    str.each_char do |char|
+      score += point_values[char.upcase]
+    end
+    score
+  end
+
+  def read_score_with_multipliers(str, arr)
+    score = 0
+    for i in 0..(str.length - 1)
+      score += (point_values[str[i].upcase] * arr[i])
+    end
+    score
+  end
 end
